bcf08b150548dc44cec06df17c2e82c094940780
MoonHo <75512752+monho@users.noreply.github.com>
2025-08-16 09:48:30 +0900

    ㅈㄴ

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -73,7 +73,7 @@ function Calendar() {
             <div key={`empty-${index}`}></div>
           ))}
           {days.map((day) => (
-            <CalendarDay key={day} day={day} isWeddingDay={day === 31} isHoliday={day === 0 }/>
+            <CalendarDay key={day} day={day} isWeddingDay={day === 30} isHoliday={day === 0 }/>
           ))}
         </div>
       </div>
